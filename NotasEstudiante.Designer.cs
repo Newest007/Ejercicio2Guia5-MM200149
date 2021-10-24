ebb3310507484954f0dcd7f66d4e742d5c95b697
Arian <adilsonarian@outlook.com>
2021-10-23 20:42:05 -0600

    Interfaz del form NotasEstudiantes completo

--- NotasEstudiante.Designer.cs
+++ NotasEstudiante.Designer.cs
@@ -38,12 +38,24 @@ namespace Ejercicio2___Guía5_MM200149
             this.label6 = new System.Windows.Forms.Label();
             this.txtnota1 = new System.Windows.Forms.MaskedTextBox();
             this.txtnota2 = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.txtnota3 = new System.Windows.Forms.MaskedTextBox();
             this.txtperiodo1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.txtnota4 = new System.Windows.Forms.MaskedTextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtnota5 = new System.Windows.Forms.MaskedTextBox();
+            this.txtperiodo2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtnota6 = new System.Windows.Forms.MaskedTextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtnota7 = new System.Windows.Forms.MaskedTextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtperiodo3 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textnotaFinal = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,7 +67,7 @@ namespace Ejercicio2___Guía5_MM200149
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(582, 100);
+            this.panel1.Size = new System.Drawing.Size(628, 100);
             this.panel1.TabIndex = 1;
             // 
             // label1
@@ -96,7 +108,7 @@ namespace Ejercicio2___Guía5_MM200149
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Location = new System.Drawing.Point(181, 269);
+            this.label3.Location = new System.Drawing.Point(187, 269);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(96, 28);
             this.label3.TabIndex = 6;
@@ -107,7 +119,7 @@ namespace Ejercicio2___Guía5_MM200149
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label5.Location = new System.Drawing.Point(118, 173);
+            this.label5.Location = new System.Drawing.Point(124, 173);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(159, 28);
             this.label5.TabIndex = 7;
@@ -118,7 +130,7 @@ namespace Ejercicio2___Guía5_MM200149
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label6.Location = new System.Drawing.Point(94, 221);
+            this.label6.Location = new System.Drawing.Point(100, 221);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(183, 28);
             this.label6.TabIndex = 8;
@@ -127,30 +139,30 @@ namespace Ejercicio2___Guía5_MM200149
             // txtnota1
             // 
             this.txtnota1.Location = new System.Drawing.Point(317, 170);
-            this.txtnota1.Mask = "0.0";
+            this.txtnota1.Mask = "0.00";
             this.txtnota1.Name = "txtnota1";
             this.txtnota1.Size = new System.Drawing.Size(55, 36);
             this.txtnota1.TabIndex = 9;
             // 
             // txtnota2
             // 
-            this.txtnota2.Location = new System.Drawing.Point(317, 219);
-            this.txtnota2.Mask = "0.0";
+            this.txtnota2.Location = new System.Drawing.Point(317, 218);
+            this.txtnota2.Mask = "0.00";
             this.txtnota2.Name = "txtnota2";
             this.txtnota2.Size = new System.Drawing.Size(55, 36);
             this.txtnota2.TabIndex = 10;
             // 
-            // maskedTextBox3
+            // txtnota3
             // 
-            this.maskedTextBox3.Location = new System.Drawing.Point(317, 317);
-            this.maskedTextBox3.Mask = "0.0";
-            this.maskedTextBox3.Name = "maskedTextBox3";
-            this.maskedTextBox3.Size = new System.Drawing.Size(55, 36);
-            this.maskedTextBox3.TabIndex = 11;
+            this.txtnota3.Location = new System.Drawing.Point(317, 314);
+            this.txtnota3.Mask = "0.00";
+            this.txtnota3.Name = "txtnota3";
+            this.txtnota3.Size = new System.Drawing.Size(55, 36);
+            this.txtnota3.TabIndex = 11;
             // 
             // txtperiodo1
             // 
-            this.txtperiodo1.Location = new System.Drawing.Point(317, 268);
+            this.txtperiodo1.Location = new System.Drawing.Point(317, 266);
             this.txtperiodo1.Name = "txtperiodo1";
             this.txtperiodo1.ReadOnly = true;
             this.txtperiodo1.Size = new System.Drawing.Size(55, 36);
@@ -162,7 +174,7 @@ namespace Ejercicio2___Guía5_MM200149
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.Control;
-            this.button2.Location = new System.Drawing.Point(416, 269);
+            this.button2.Location = new System.Drawing.Point(451, 256);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(116, 51);
             this.button2.TabIndex = 15;
@@ -175,43 +187,169 @@ namespace Ejercicio2___Guía5_MM200149
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label7.Location = new System.Drawing.Point(118, 317);
+            this.label7.Location = new System.Drawing.Point(124, 317);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(159, 28);
             this.label7.TabIndex = 16;
             this.label7.Text = "Desafío Práctico 2";
             // 
-            // maskedTextBox4
+            // txtnota4
             // 
-            this.maskedTextBox4.Location = new System.Drawing.Point(317, 366);
-            this.maskedTextBox4.Mask = "0.0";
-            this.maskedTextBox4.Name = "maskedTextBox4";
-            this.maskedTextBox4.Size = new System.Drawing.Size(55, 36);
-            this.maskedTextBox4.TabIndex = 17;
+            this.txtnota4.Location = new System.Drawing.Point(317, 362);
+            this.txtnota4.Mask = "0.00";
+            this.txtnota4.Name = "txtnota4";
+            this.txtnota4.Size = new System.Drawing.Size(55, 36);
+            this.txtnota4.TabIndex = 17;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label8.Location = new System.Drawing.Point(124, 365);
+            this.label8.Location = new System.Drawing.Point(130, 365);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(153, 28);
             this.label8.TabIndex = 18;
             this.label8.Text = "Guía de Ejercicios";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label9.Location = new System.Drawing.Point(187, 413);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 28);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Periodo 2:";
+            // 
+            // txtnota5
+            // 
+            this.txtnota5.Location = new System.Drawing.Point(317, 458);
+            this.txtnota5.Mask = "0.00";
+            this.txtnota5.Name = "txtnota5";
+            this.txtnota5.Size = new System.Drawing.Size(55, 36);
+            this.txtnota5.TabIndex = 20;
+            // 
+            // txtperiodo2
+            // 
+            this.txtperiodo2.Location = new System.Drawing.Point(317, 410);
+            this.txtperiodo2.Name = "txtperiodo2";
+            this.txtperiodo2.ReadOnly = true;
+            this.txtperiodo2.Size = new System.Drawing.Size(55, 36);
+            this.txtperiodo2.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label10.Location = new System.Drawing.Point(124, 461);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(159, 28);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Desafío Práctico 3";
+            // 
+            // txtnota6
+            // 
+            this.txtnota6.Location = new System.Drawing.Point(317, 506);
+            this.txtnota6.Mask = "0.00";
+            this.txtnota6.Name = "txtnota6";
+            this.txtnota6.Size = new System.Drawing.Size(55, 36);
+            this.txtnota6.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label11.Location = new System.Drawing.Point(100, 509);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(183, 28);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Control de Lectura 2";
+            // 
+            // txtnota7
+            // 
+            this.txtnota7.Location = new System.Drawing.Point(317, 554);
+            this.txtnota7.Mask = "0.00";
+            this.txtnota7.Name = "txtnota7";
+            this.txtnota7.Size = new System.Drawing.Size(55, 36);
+            this.txtnota7.TabIndex = 25;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label12.Location = new System.Drawing.Point(109, 557);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(174, 28);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "Proyecto de catedra";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label13.Location = new System.Drawing.Point(187, 605);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(96, 28);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "Periodo 3:";
+            // 
+            // txtperiodo3
+            // 
+            this.txtperiodo3.Location = new System.Drawing.Point(317, 602);
+            this.txtperiodo3.Name = "txtperiodo3";
+            this.txtperiodo3.ReadOnly = true;
+            this.txtperiodo3.Size = new System.Drawing.Size(55, 36);
+            this.txtperiodo3.TabIndex = 28;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label14.Location = new System.Drawing.Point(463, 322);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(93, 28);
+            this.label14.TabIndex = 29;
+            this.label14.Text = "Nota final";
+            // 
+            // textnotaFinal
+            // 
+            this.textnotaFinal.Location = new System.Drawing.Point(482, 357);
+            this.textnotaFinal.Name = "textnotaFinal";
+            this.textnotaFinal.ReadOnly = true;
+            this.textnotaFinal.Size = new System.Drawing.Size(55, 36);
+            this.textnotaFinal.TabIndex = 30;
             // 
             // NotasEstudiante
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(64)))), ((int)(((byte)(83)))));
-            this.ClientSize = new System.Drawing.Size(582, 463);
+            this.ClientSize = new System.Drawing.Size(628, 681);
+            this.Controls.Add(this.textnotaFinal);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.txtperiodo3);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtnota7);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtnota6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtperiodo2);
+            this.Controls.Add(this.txtnota5);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.maskedTextBox4);
+            this.Controls.Add(this.txtnota4);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.txtperiodo1);
-            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.txtnota3);
             this.Controls.Add(this.txtnota2);
             this.Controls.Add(this.txtnota1);
             this.Controls.Add(this.label6);
@@ -243,11 +381,23 @@ namespace Ejercicio2___Guía5_MM200149
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.MaskedTextBox txtnota1;
         private System.Windows.Forms.MaskedTextBox txtnota2;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox txtnota3;
         private System.Windows.Forms.TextBox txtperiodo1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.MaskedTextBox txtnota4;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.MaskedTextBox txtnota5;
+        private System.Windows.Forms.TextBox txtperiodo2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.MaskedTextBox txtnota6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.MaskedTextBox txtnota7;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtperiodo3;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textnotaFinal;
     }
 }
--- NotasEstudiante.cs
+++ NotasEstudiante.cs
@@ -24,6 +24,19 @@ namespace Ejercicio2___Guía5_MM200149
 
         private void button2_Click(object sender, EventArgs e)
         {
+            double nota1 = Convert.ToDouble(txtnota1.Text);
+            double nota2 = Convert.ToDouble(txtnota2.Text);
+            double periodo1 = (nota1 + nota2) / 2;
+            txtperiodo1.Text = Convert.ToString(periodo1);
+            double nota3 = Convert.ToDouble(txtnota3.Text);
+            double nota4 = Convert.ToDouble(txtnota4.Text);
+            double periodo2 = (nota3 + nota4) / 2;
+            txtperiodo2.Text = Convert.ToString(periodo2);
+            double nota5 = Convert.ToDouble(txtnota5.Text);
+            double nota6 = Convert.ToDouble(txtnota6.Text);
+            double nota7 = Convert.ToDouble(txtnota7.Text);
+            double periodo3 = (nota5 + nota6 + nota7) / 3;
+            txtperiodo3.Text = Convert.ToString(periodo3);
 
         }
     }
